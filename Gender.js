b1b312a71e05a319545c38f7f157b08ce352e64b
Mario Metzler <m.metzler@me.com>
2020-05-15 14:48:19 +0200

    Changed script origin

--- Gender.js
+++ Gender.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://raw.githubusercontent.com/mametz/IAT/master/quiat8.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
 
 	var posWords = API.shuffle([
